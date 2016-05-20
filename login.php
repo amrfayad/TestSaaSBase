e5d46322ddab6e77ecc2cb9e072a7f5a65f6967c
Yasmine <yasminemohamed2093@gmail.com>
2016-05-20 15:21:04 +0200

    test

--- login.php
+++ login.php
@@ -1,7 +1,15 @@
-<html>
-    <head>
-        <title>Ajax Example</title>
-        <link rel="stylesheet" href="./boot/bootstrap.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Login</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
         <script src="js/jquery.min.js"></script>
         <script src="js/md5.min.js"></script>
         <script>
@@ -50,25 +58,22 @@
             }
         </script>
     <body>
-        <form method="post">
-            <table class="table">
-                <tr><td>User Email</td>
-                    <td><input type="text" name="usname" id ="email"></td>
-                </tr>
-                <tr>
-                    <td>Password</td>    
-                    <td><input type="password" name="pass" id ="pass"></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="button" name="submit" value="LogIn" onclick="loginMessage()"></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="button" name="submit" value="SignUp" onclick="signupMessage()"></td>
-                </tr>
-            </table>
-        </form> 
+    <br><br>
+    <h1 align="center">Login Form</h1>
+    <div class="container">
+        <div class="form">
+
+            <div class="form-group">
+                <label for="Email1">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="Password">Password</label>
+                <input type="password" class="form-control" id="pass" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary" onclick="loginMessage()">Login</button>
+        </div>
+    </div>
 
 
     </body>
