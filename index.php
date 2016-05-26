34335d77087568f0231baa8c9e4ef5b7581a0c15
amrfayad <amr.fci2007@gmail.com>
2016-05-26 13:04:55 +0200

    Move home to index.php

--- index.php
+++ index.php
@@ -1,76 +1,34 @@
-<html>
-    <head>
-        <title>Ajax Example</title>
-        <link rel="stylesheet" href="./css/bootstrap.css">
-        <script src="js/jquery.min.js"></script>
-        <script src="js/md5.min.js"></script>
-        <script>
-            function loginMessage() {
-                var email = $('#email').val();
-                var pass = md5($('#pass').val());
-                var key = "e10adc3949ba59abbe56e057f20f883e";
-                var action = "login"
-                var data=action+''+email+''+pass+''+key;
-                var hashed = md5(data);
-                var userData = {
-                    action: action,
-                    email: email,
-                    pass: pass,
-                };
-                $.ajax(
-                        {
-                            type: 'post',
-                            url: 'http://localhost/SaaSBase/',
-                            data: {
-                                data: userData,
-                                hash: hashed,
-                            },
-                            success: function (data)
-                            {
-                                alert(data);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Home</title>
 
-                            }
-                        });
-            }
-            function signupMessage() {
-                $.ajax(
-                        {
-                            type: 'post',
-                            url: 'http://localhost/SaaSBase/index.php',
-                            data: {
-                                action: "signup",
-                                name: "amr",
-                                pass: "123456"
-                            },
-                            success: function (data)
-                            {
-                                alert(data);
-                            }
-                        });
-            }
-        </script>
-    <body>
-        <form method="post">
-            <table class="table">
-                <tr><td>User Email</td>
-                    <td><input type="text" name="usname" id ="email"></td>
-                </tr>
-                <tr>
-                    <td>Password</td>    
-                    <td><input type="password" name="pass" id ="pass"></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="button" name="submit" value="LogIn" onclick="loginMessage()"></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="button" name="submit" value="SignUp" onclick="signupMessage()"></td>
-                </tr>
-            </table>
-        </form> 
-
-
-    </body>
-
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<br><br><br>
+<div class="container">
+	
+	<a href="register.php" class="btn btn-primary">Register</a>
+	<a href="login.php" class="btn btn-primary">Login</a>
+	<a href="invite.php" class="btn btn-primary">Invite</a>
+	<br><br>
+	<a href="accept_invitation.php" class="btn btn-primary">Accept Invitation</a>
+	<a href="change_password.php" class="btn btn-primary">Change Password</a>
+	<a href="forgot_password.php" class="btn btn-primary">Forget Password</a>
+	<br><br>
+	<a href="assign_billing.php" class="btn btn-primary">Assign Billing</a>
+	<a href="reset.php" class="btn btn-primary">Reset Password</a>
+	<a href="add_role.php" class="btn btn-primary">Add Role</a>
+	<br><br>
+	<a href="active&deactive_user.php" class="btn btn-primary">Active & Deactive User</a>
+	<a href="enable&disable_role.php" class="btn btn-primary">Enable & Disable Role</a>
+	<a href="userProfile.php" class="btn btn-primary">User Profile</a>
+</div>	
+</body>
 </html>
